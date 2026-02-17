e56e569a08386b01b059ebc9857aa20c55a151fa
ss6909 <ss6909@srmist.edu.in>
2026-02-17 15:23:27 +0530

    UC2-Checking if it is a palindrome

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
-    public static void main(String[] args){
-        System.out.println("Welcome to Palindrome Checker App Management System");
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Palindrome Checker Management App");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String word = scanner.nextLine();
+
+        String reversed = "";
+        boolean isPalindrome = true;
+
+        word = word.toLowerCase();
+
+        for (int i = 0; i < word.length() / 2; i++) {
+            if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println(word + " is a Palindrome.");
+        } else {
+            System.out.println(word + " is NOT a Palindrome.");
+        }
+
+        scanner.close();
     }
 }
